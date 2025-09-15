e27c371e73c66b6cc5466a94fbb197fee4adff7d
clocloh2005-beep <cloclo.h2005@gmail.com>
2025-09-15 10:51:05 +0200

    exam

---
+++ exercice4/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+func CountIf (f func(string) bool, tab []string ) int {
+	count := 0
+	if i:= 0 ; i < len(tab), i++ {
+		return count ++
+	} 
+
+	if f(a(tab[i])) {
+		return false
+	} 
+}
+
+func main(){
+	fmt.Println(CountIf)
+}
